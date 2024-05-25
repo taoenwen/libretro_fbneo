94ffde3762d0fc051ceb563e5d91229a3bc83324
taoenwen <67533945+taoenwen@users.noreply.github.com>
2024-05-25 23:34:58 +0800

    libretro.cpp : Further judgment when the loaded set is not in the list

--- src/burner/libretro/libretro.cpp
+++ src/burner/libretro/libretro.cpp
@@ -2237,11 +2237,10 @@ end:
 static int retro_dat_romset_path(const struct retro_game_info* info, char* pszRomsetPath)
 {
 	INT32 nRet = 0;	// 1: romdata; 2: ips;
+	char szDatDir[MAX_PATH] = { 0 }, szRomset[128] = { 0 }, * pszTmp = NULL;
 
 	if (0 == strcmp(strrchr(info->path, '.'), ".dat"))
 	{
-		char szDatDir[MAX_PATH] = { 0 }, szRomset[100] = { 0 }, * pszTmp = NULL;
-
 		memset(szRomdataName, 0, MAX_PATH);
 		strcpy(szRomdataName, info->path);				// romdata_dir/romdata.dat
 
@@ -2274,7 +2273,37 @@ static int retro_dat_romset_path(const struct retro_game_info* info, char* pszRo
 		_stprintf(pszRomsetPath, _T("%s%c%s"), szDatDir, PATH_DEFAULT_SLASH_C(), szRomset);
 	}
 	else
+	{
 		strcpy(pszRomsetPath, info->path);
+		extract_basename(szRomset, info->path, sizeof(szRomset), "");
+
+		// Not found in the list of games
+		// Probably the set of romdata
+		if (~0U == BurnDrvGetIndexByName(szRomset))
+		{
+			const char* dir = NULL;
+			char szSysDir[MAX_PATH] = { 0 };
+
+			extract_directory(szDatDir, info->path, sizeof(szDatDir));
+			memset(szRomdataName, 0, MAX_PATH);
+			_stprintf(szRomdataName, _T("%s%c%s.dat"), szDatDir, PATH_DEFAULT_SLASH_C(), szRomset);
+
+			if (NULL != (pszTmp = RomdataGetDrvName()))
+				nRet = 1;
+			else
+			{
+				if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
+				{
+					memcpy(szSysDir, dir, sizeof(szSysDir));
+					memset(szRomdataName, 0, MAX_PATH);
+					_stprintf(szRomdataName, _T("%s%cfbneo%cromdata%c%s.dat"), szSysDir, PATH_DEFAULT_SLASH_C(), PATH_DEFAULT_SLASH_C(), PATH_DEFAULT_SLASH_C(), szRomset);
+
+					if (NULL != (pszTmp = RomdataGetDrvName()))
+						nRet = 1;
+				}
+			}
+		}
+	}
 
 	return nRet;
 }
